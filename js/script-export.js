cccfe739a36294134be9bd063b0a65408b1f93c3
kevingrillet <kevin.grillet@gmail.com>
2021-09-18 23:33:51 +0200

    Add export as png

---
+++ js/script-export.js
@@ -0,0 +1,14 @@
+document.querySelector('#btnExport').onclick = function (e) {
+    /* Not working properly due to Clip-path
+    html2canvas(document.querySelector("#LabPath")).then(canvas => {
+        document.body.appendChild(canvas)
+    });
+    */
+
+    console.log(e.target);
+
+    domtoimage.toBlob(document.querySelector("#LabPath"), { bgcolor: '#2e2b33'})
+        .then(function (blob) {
+            saveAs(blob, e.target.title+'.png');
+        });
+}
