8db1e1d20d82c7e3c3fd21ae85a71d6798e2360c
kevingrillet <kevin.grillet@gmail.com>
2021-09-18 16:49:37 +0200

    Drag & Drop
    Drag & Drop swap

--- www/script.js
+++ www/script.js
@@ -33,3 +33,43 @@ window.addEventListener("DOMContentLoaded", (event) => {
         e.oncontextmenu = newClickHandeler; // Right click -> Context Menu
     });
 });
+
+// Drag & Drop!
+var dragged;
+document.addEventListener("drag", function (event) {
+}, false);
+
+document.addEventListener("dragstart", function (event) {
+    dragged = event.target;
+    event.target.style.opacity = .5;
+}, false);
+
+document.addEventListener("dragend", function (event) {
+    event.target.style.opacity = "";
+}, false);
+
+document.addEventListener("dragover", function (event) {
+    event.preventDefault();
+}, false);
+
+document.addEventListener("dragenter", function (event) {
+    if (event.target.tagName.toLowerCase() == "span" && event.target != dragged) {
+        event.target.style.background = "LimeGreen";
+    }
+}, false);
+
+document.addEventListener("dragleave", function (event) {
+    if (event.target.tagName.toLowerCase() == "span" && event.target != dragged) {
+        event.target.style.background = "";
+    }
+}, false);
+
+document.addEventListener("drop", function (event) {
+    event.preventDefault();
+    if (event.target.tagName.toLowerCase() == "span" && event.target != dragged) {
+        event.target.style.background = "";
+        let tmpClass = dragged.className;
+        dragged.className = event.target.className;
+        event.target.className = tmpClass;
+    }
+}, false);
