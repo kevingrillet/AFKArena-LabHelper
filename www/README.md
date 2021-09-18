183581b93c3e349f96831be50ccb824ee5edb68c
kevingrillet <kevin.grillet@gmail.com>
2021-09-18 20:20:30 +0200

    WIP
    Refacto
    Save & Load

--- www/README.md
+++ www/README.md
@@ -6,4 +6,4 @@
 - [x] Drag & Drop: <https://www.w3schools.com/html/html5_draganddrop.asp>
 - [x] Drag & Drop Swap Items: <https://developer.mozilla.org/fr/docs/Web/API/Document/drag_event>
 - [x] Custom right-click menu to change item: <https://itnext.io/how-to-create-a-custom-right-click-menu-with-javascript-9c368bb58724>
-- [ ] Text output? Need to see where it will be used :)
+- [x] Text output? Need to see where it will be used :) <https://github.com/eligrey/FileSaver.js>
---
+++ www/js/script-importexport.js
@@ -0,0 +1,104 @@
+const newLineTiles = [1, 3, 6, 8, 11, 13, 16, 18, 21, 23, 24]
+
+document.querySelector('#btnSave').onclick = function () {
+    let output = "";
+    document.querySelectorAll('.floor').forEach((f) => {
+        output += " **" + f.firstElementChild.innerHTML + "**\n";
+        let cpt = 0;
+        f.querySelectorAll('span').forEach((s) => {
+            switch (s.className) {
+                case 'yellow':
+                    output += 'Boss';
+                    break;
+                case 'black':
+                    output += 'Wrizz';
+                    break;
+                case 'red':
+                    output += 'Praetorian Guard';
+                    break;
+                case 'lightbrown':
+                    output += 'Guard';
+                    break;
+                case 'brown':
+                    output += 'Roamer';
+                    break;
+                case 'blue':
+                    output += 'Abandoned Wagon';
+                    break;
+                case 'lightblue':
+                    output += 'Fountain of Vitality';
+                    break;
+                case 'pink':
+                    output += 'Mystic';
+                    break;
+                case 'white':
+                    output += 'Start';
+                    break;
+            }
+            if (newLineTiles.includes(++cpt)) {
+                output += '\n';
+            } else {
+                output += ', ';
+            }
+        });
+        output += "\n";
+    });
+    let blob = new Blob([output], { type: "text/plain;charset=utf-8" });
+    saveAs(blob, "arcane-labyrinth.txt");
+}
+
+document.querySelector('#btnLoad').onclick = function () {
+    document.querySelector('#fileInput').click();
+}
+
+document.querySelector('#fileInput').onchange = function (file) {
+    let floor = null;
+    let curFloor = 0;
+    let curFloorCell = 0;
+    let fr = new FileReader();
+    fr.onload = function () {
+        let lines = fr.result.split('\n');
+        for (let i = 0; i < lines.length; i++) {
+            if (!floor) {
+                floor = document.querySelectorAll('.floor')[curFloor++];
+                curFloorCell = 0;
+            } else if (lines[i] == "") {
+                floor = null;
+            } else {
+                let elt = lines[i].split(', ');
+                for (let e = 0; e < elt.length; e++) {
+                    switch (elt[e]) {
+                        case 'Boss':
+                            floor.querySelectorAll('span')[curFloorCell++].className = 'yellow';
+                            break;
+                        case 'Wrizz':
+                            floor.querySelectorAll('span')[curFloorCell++].className = 'black';
+                            break;
+                        case 'Praetorian Guard':
+                            floor.querySelectorAll('span')[curFloorCell++].className = 'red';
+                            break;
+                        case 'Guard':
+                            floor.querySelectorAll('span')[curFloorCell++].className = 'lightbrown';
+                            break;
+                        case 'Roamer':
+                            floor.querySelectorAll('span')[curFloorCell++].className = 'brown';
+                            break;
+                        case 'Abandoned Wagon':
+                            floor.querySelectorAll('span')[curFloorCell++].className = 'blue';
+                            break;
+                        case 'Fountain of Vitality':
+                            floor.querySelectorAll('span')[curFloorCell++].className = 'lightblue';
+                            break;
+                        case 'Mystic':
+                            floor.querySelectorAll('span')[curFloorCell++].className = 'pink';
+                            break;
+                        case 'Start':
+                            floor.querySelectorAll('span')[curFloorCell++].className = 'white';
+                            break;
+                    }
+                }
+            }
+        }
+    }
+    fr.readAsText(file.target.files[0]);
+}
