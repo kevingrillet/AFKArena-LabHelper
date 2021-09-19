36c3d29fd7c407137e7d20c53fd025f045a0425a
kevingrillet <kevin.grillet@gmail.com>
2021-09-19 11:42:33 +0200

    Dismal drag & drop images

--- js/script-dragdrop.js
+++ js/script-dragdrop.js
@@ -25,26 +25,44 @@ document.addEventListener('dragover', function (event) {
 }, false);
 
 document.addEventListener('dragenter', function (event) {
-    if (event.target.tagName.toLowerCase() == 'span'
-        && event.target.parentElement.className == 'floor'
-        && event.target != dragged) {
+    if (event.target != dragged
+        && (dragged.tagName.toLowerCase() == 'span'
+            && event.target.tagName.toLowerCase() == 'span'
+            && event.target.parentElement.className == 'floor')
+        || (dragged.tagName.toLowerCase() == 'img'
+            && event.target.tagName.toLowerCase() == 'img'
+            && dragged.parentElement.parentElement.parentElement.className == event.target.parentElement.parentElement.parentElement.className
+        )
+    ) {
         event.target.style.opacity = .8;
     }
 }, false);
 
 document.addEventListener('dragleave', function (event) {
-    if (event.target.tagName.toLowerCase() == 'span'
-        && event.target.parentElement.className == 'floor'
-        && event.target != dragged) {
+    if (event.target != dragged
+        && (dragged.tagName.toLowerCase() == 'span'
+            && event.target.tagName.toLowerCase() == 'span'
+            && event.target.parentElement.className == 'floor')
+        || (dragged.tagName.toLowerCase() == 'img'
+            && event.target.tagName.toLowerCase() == 'img'
+            && dragged.parentElement.parentElement.parentElement.className == event.target.parentElement.parentElement.parentElement.className
+        )
+    ) {
         event.target.style.opacity = '';
     }
 }, false);
 
 document.addEventListener('drop', function (event) {
     event.preventDefault();
-    if (event.target.tagName.toLowerCase() == 'span'
-        && event.target.parentElement.className == 'floor'
-        && event.target != dragged) {
+    if (event.target != dragged
+        && (dragged.tagName.toLowerCase() == 'span'
+            && event.target.tagName.toLowerCase() == 'span'
+            && event.target.parentElement.className == 'floor')
+        || (dragged.tagName.toLowerCase() == 'img'
+            && event.target.tagName.toLowerCase() == 'img'
+            && dragged.parentElement.parentElement.parentElement.className == event.target.parentElement.parentElement.parentElement.className
+        )
+    ) {
         dragged.style.opacity = '';
         event.target.style.opacity = '';
         swapElements(dragged, event.target);
