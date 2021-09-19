c620c0f3d07fabc595ebf4088261a3f5a5c771f1
kevingrillet <kevin.grillet@gmail.com>
2021-09-19 11:26:11 +0200

    Cleaner Drag & Drop

--- js/script-dragdrop.js
+++ js/script-dragdrop.js
@@ -8,45 +8,47 @@ function swapElements(element1, element2) {
     element2.parentNode.replaceChild(clonedElement1, element2);
 }
 
-document.addEventListener("drag", function (event) {
+document.addEventListener('drag', function (event) {
 }, false);
 
-document.addEventListener("dragstart", function (event) {
+document.addEventListener('dragstart', function (event) {
     dragged = event.target;
     event.target.style.opacity = .5;
 }, false);
 
-document.addEventListener("dragend", function (event) {
-    event.target.style.opacity = "";
+document.addEventListener('dragend', function (event) {
+    event.target.style.opacity = '';
 }, false);
 
-document.addEventListener("dragover", function (event) {
+document.addEventListener('dragover', function (event) {
     event.preventDefault();
 }, false);
 
-document.addEventListener("dragenter", function (event) {
-    if (event.target.tagName.toLowerCase() == "span"
-        && event.target.parentElement.className == "floor"
+document.addEventListener('dragenter', function (event) {
+    if (event.target.tagName.toLowerCase() == 'span'
+        && event.target.parentElement.className == 'floor'
         && event.target != dragged) {
-        event.target.style.background = "LimeGreen";
+
+        event.target.style.opacity = .8;
+        //event.target.style.background = 'LimeGreen';
     }
 }, false);
 
-document.addEventListener("dragleave", function (event) {
-    if (event.target.tagName.toLowerCase() == "span"
-        && event.target.parentElement.className == "floor"
+document.addEventListener('dragleave', function (event) {
+    if (event.target.tagName.toLowerCase() == 'span'
+        && event.target.parentElement.className == 'floor'
         && event.target != dragged) {
-        event.target.style.background = "";
+        event.target.style.opacity = '';
     }
 }, false);
 
-document.addEventListener("drop", function (event) {
+document.addEventListener('drop', function (event) {
     event.preventDefault();
-    if (event.target.tagName.toLowerCase() == "span"
-        && event.target.parentElement.className == "floor"
+    if (event.target.tagName.toLowerCase() == 'span'
+        && event.target.parentElement.className == 'floor'
         && event.target != dragged) {
-        dragged.style.opacity = "";
-        event.target.style.background = "";
+        dragged.style.opacity = '';
+        event.target.style.opacity = '';
         swapElements(dragged, event.target);
     }
 }, false);
